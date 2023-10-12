978f9c8486a8248115ac58449afb7baae97f2799
hanbu <buhan970731@gmail.com>
2023-10-12 19:20:25 +0800

    todo: select network

--- lib/utils/config/config.dart
+++ lib/utils/config/config.dart
@@ -1,13 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import '/utils/log/logger.dart';
-// import 'package:flutter_openim_sdk/flutter_openim_sdk.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:vara_sdk/api/types/networkParams.dart';
-import 'package:vara_sdk/polkawallet_sdk.dart';
-import 'package:vara_sdk/storage/keyring.dart';
+
 import '../../models/wallet/wallet_types.dart';
 import '../../models/wallet/wallets.dart';
 import '../../models/wallet/wallets_group.dart';
@@ -33,25 +26,7 @@ class Config {
       Hive.registerAdapter(WalletReadAdapter());
     } catch (_) {}
 
-    // init tdlib
-
     runApp();
-
-    // screen dirction
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-      DeviceOrientation.portraitDown,
-    ]);
-
-    // systemUIOverlayStyle transparent（Android）
-    var brightness = Platform.isAndroid ? Brightness.dark : Brightness.light;
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarBrightness: brightness,
-      statusBarIconBrightness: brightness,
-    ));
-
-    // FlutterBugly.init(androidAppId: "", iOSAppId: "");
   }
 
   static late String cachePath;
@@ -60,9 +35,4 @@ class Config {
 
   /// 全局字体size
   static const double textScaleFactor = 1.0;
-
-  /// 秘钥
-  static const secret = 'tuoyun';
-
-  static const mapKey = '';
 }
